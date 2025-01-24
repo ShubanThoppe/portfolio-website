73ce609a327d27036bae6df5f7145df6cb827a06
ShubanThoppe <163137177+ShubanThoppe@users.noreply.github.com>
2025-01-24 23:00:27 +0000

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Java these days but I'm trying to get more into Web Development.
+    I've made a couple of projects so far all which can be seen on my <a href="https://github.com/ShubanThoppe" target="_blank">Github</a>!. I'm currently working on a project with the company Nativz Gaming as part of one of my college modules, to build a Tournament bracketing system to use in their E-Sports events! Looking forward to having that finished!`,
+  "cs-degree": `This is my Leaving Cert. I hung it on the wall because I'm proud of it. I ended up getting 613 points with 5 H1's and 1 H2. Currently I'm a 2nd Year studying Computer Science in Trinity College.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube and Netflix.`,
+  tv: `That's my TV...that does nothing cool, yet...`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. I'm always trying to think of ideas for a startup.`,
+  resume: `This is my desk and on it is my resume. <a href="./ShubanThoppeCV.pdf" target="_blank">Check it out?</a>
+  Contact me at thoppeshuban@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript adn the text is rendered with HTML/CSS.`,
+  library: `I've learnt alot of programming languauges over the years - HTML, Javascript, CSS, Java, VHDL, Pyhton, C, Kotlin and (Java) Processing. I've also used a bunch of IDE's succh as Eclipse, Vivado, Android Studio, Visual Studio. I'm always learning new languages through the projects and assignemnts I do.`,
+  exit: `If you want to exit Shuban Thoppe's portfolio, just close the tab.`,
+  chest: `I'm a very outdoors person. Some of my hobbies include Gaelic Football and Hurling. I've recently also picked up Bouldering too. I'm also known to be a frequent gym-goer and spend couple hours out of my day there!`,
+};
